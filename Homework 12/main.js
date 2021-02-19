2fbad05ce9c47091eafb56caa23ea2b0d6bf12bf
AndriyIvaniukh <andriyivaniukh@gmail.com>
2021-02-19 16:47:15 +0200

    update hm12

---
+++ Homework 12/main.js
@@ -0,0 +1,26 @@
+// --Завантажити з мережі будь-який шаблон сайту. Підключити до нього свій скріпт-файл. У файлі прописати наступні доступи та дії
+//  - знайти всі елементі, які мають class
+//  - знайти всі параграфи ,та змінити текст на hello oktenweb!
+//  - знайти всі div та змінити ім колір на червоний
+
+// let byClass = document.getElementsByTagName('*');
+// for (let text of byClass) {
+//     if (text.getAttribute('class')) {
+//         console.log(text);
+//     }
+
+// }
+
+
+
+// let byP = document.getElementsByTagName('p');
+// for (let text of byP) {
+//     text.innerText = 'Okten Web!';
+
+// }
+
+// let byDiv = document.getElementsByTagName('div');
+// for (let text of byDiv) {
+//     text.style.background = 'red';
+
+// }
