07e9b0676535432591e77d245fcf9735be7ed781
AndriyIvaniukh <andriyivaniukh@gmail.com>
2021-03-24 16:42:10 +0200

    apdate hm17

--- Homework 17/main.js
+++ Homework 17/main.js
@@ -59,21 +59,107 @@
 //  За допомогою document.createElement вивести їх в браузер. 
 //  Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт в свій блок (блок в блоці).
 
-fetch(`https://jsonplaceholder.typicode.com/users`)
-    .then(value => value.json())
-    .then(value => console.log(value));
+// fetch(`https://jsonplaceholder.typicode.com/users`)
+//     .then(value => value.json())
+//     .then(values => {
+//         let div = document.createElement('div');
+
+//         for (let user of values) {
+//             let div1 = document.createElement('div');
+//             div1.innerText = `id - ${user.id}, name - ${user.name},  email - ${user.email}`;
+//             div.appendChild(div1);
+//         }
+
+//         document.body.appendChild(div);
+//     });
 
 // - за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі posts. 
 // За допомогою document.createElement вивести їх в браузер. 
 // Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт(якщо він існує) в свій блок (блок в блоці).
 
 
+// fetch(`https://jsonplaceholder.typicode.com/posts`)
+//     .then(value => value.json())
+//     .then(values => {
+//         let div = document.createElement('div');
+
+//         for (let post of values) {
+//             let div1 = document.createElement('div');
+//             let h3 = document.createElement('h3');
+//             let p1 = document.createElement('p');
+//             let p2 = document.createElement('p');
+
+//             h3.innerText = `id - ${post.id}`;
+//             p1.innerText = `title - ${post.title}`;
+//             p2.innerText = `body - ${post.body}`;
+
+//             div1.appendChild(h3);
+//             div1.appendChild(p1);
+//             div1.appendChild(p2);
+//             div.appendChild(div1);
+//         }
+
+//         document.body.appendChild(div);
+//     });
+
+
 // - за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі comments. 
 // За допомогою document.createElement вивести їх в браузер. 
 // Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт(якщо він існує) в свій блок (блок в блоці).
 
+// fetch(`https://jsonplaceholder.typicode.com/comments`)
+//     .then(value => value.json())
+//     .then(values => {
+//         let div = document.createElement('div');
+
+//         for (let comment of values) {
+//             let div1 = document.createElement('div');
+//             let h3 = document.createElement('h3');
+//             let p1 = document.createElement('p');
+//             let p2 = document.createElement('p');
+
+//             h3.innerText = `id - ${comment.id}`;
+//             p1.innerText = `name - ${comment.name}`;
+//             p2.innerText = `body - ${comment.body}`;
+
+//             div1.appendChild(h3);
+//             div1.appendChild(p1);
+//             div1.appendChild(p2);
+//             div.appendChild(div1);
+//         }
+
+//         document.body.appendChild(div);
+//     });
 
 // -при помощи fetch (как в примере) получить от jsonplaceholder все posts. 
 // Внутри последнего then() сделать еще один fetch который сделает запрос и получит все comments. 
 // Объеденить соответсвующий post с соответсвующими comment и вывести в браузер. 
 // Подсказка : в каждом comment есть поле postId которое определяет какой комментарий принадлежит какому посту
+
+fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(response => response.json())
+    .then(posts => fetch(`https://jsonplaceholder.typicode.com/comments?postId=` + posts.id))
+    .then(response => response.json())
+    .then(comment => {
+        let div = document.createElement('div');
+
+        let div1 = document.createElement('div');
+        let h03 = document.createElement('h3');
+        // let h13 = document.createElement('h3');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+
+        // h13.innerText = `post ${posts[0]}`
+        h03.innerText = `id - ${comment.id}`;
+        p1.innerText = `name - ${comment.name}`;
+        p2.innerText = `body - ${comment.body}`;
+
+        div1.appendChild(h03);
+        // div1.appendChild(h13);
+        div1.appendChild(p1);
+        div1.appendChild(p2);
+        div.appendChild(div1);
+
+
+        document.body.appendChild(div);
+    });
