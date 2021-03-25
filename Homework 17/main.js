c1b24bd9a2d224188369eb9d4c0e0a3716eec4eb
AndriyIvaniukh <andriyivaniukh@gmail.com>
2021-03-25 13:35:47 +0200

    apdate HM17

--- Homework 17/main.js
+++ Homework 17/main.js
@@ -136,30 +136,50 @@
 // Объеденить соответсвующий post с соответсвующими comment и вывести в браузер. 
 // Подсказка : в каждом comment есть поле postId которое определяет какой комментарий принадлежит какому посту
 
-fetch(`https://jsonplaceholder.typicode.com/posts`)
-    .then(response => response.json())
-    .then(posts => fetch(`https://jsonplaceholder.typicode.com/comments?postId=` + posts.id))
-    .then(response => response.json())
-    .then(comment => {
-        let div = document.createElement('div');
+// fetch(`https://jsonplaceholder.typicode.com/posts`)
+//     .then(response => response.json())
+//     .then(posts => {
+//         let div = document.createElement('div');
+//         for (let post of posts) {
+//             let postDiv = document.createElement('div');
+//             let postH3 = document.createElement('h3');
+//             let postP1 = document.createElement('p');
+//             let postP2 = document.createElement('p');
 
-        let div1 = document.createElement('div');
-        let h03 = document.createElement('h3');
-        // let h13 = document.createElement('h3');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
+//             postH3.innerText = `id - ${post.id}`;
+//             postP1.innerText = `title - ${post.title}`;
+//             postP2.innerText = `body - ${post.body}`;
 
-        // h13.innerText = `post ${posts[0]}`
-        h03.innerText = `id - ${comment.id}`;
-        p1.innerText = `name - ${comment.name}`;
-        p2.innerText = `body - ${comment.body}`;
+//             postDiv.appendChild(postH3);
+//             postDiv.appendChild(postP1);
+//             postDiv.appendChild(postP2);
+//             fetch(`https://jsonplaceholder.typicode.com/comments?postId=` + post.id)
+//                 .then(response => response.json())
+//                 .then(comments => {
+//                     let commentDiv = document.createElement('div');
 
-        div1.appendChild(h03);
-        // div1.appendChild(h13);
-        div1.appendChild(p1);
-        div1.appendChild(p2);
-        div.appendChild(div1);
+//                     for (let comment of comments) {
+//                         let div1 = document.createElement('div');
+//                         let h3 = document.createElement('h3');
+//                         let p1 = document.createElement('p');
+//                         let p2 = document.createElement('p');
+
+//                         h3.innerText = `postId - ${comment.postId}`;
+//                         p1.innerText = `name - ${comment.name}`;
+//                         p2.innerText = `body - ${comment.body}`;
+
+//                         div1.appendChild(h3);
+//                         div1.appendChild(p1);
+//                         div1.appendChild(p2);
+//                         commentDiv.appendChild(div1);
+
+//                     }
+//                     postDiv.appendChild(commentDiv);
+//                 })
+//             div.appendChild(postDiv);
+
+//         }
+//         document.body.appendChild(div);
 
 
-        document.body.appendChild(div);
-    });
+//     });
