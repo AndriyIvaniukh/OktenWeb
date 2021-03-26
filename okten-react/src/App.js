d44659b38929baaf9b3e910cbf4d2c5501fe5472
AndriyIvaniukh <andriyivaniukh@gmail.com>
2021-03-26 11:23:35 +0200

    Fist project React

---
+++ okten-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+    const msg = 'hello beach';
+  return (
+    <div>
+        <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
